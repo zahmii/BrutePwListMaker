bc98b84800de826841326d59178c9c8781eb71ee
zahmii <46228534+zahmii@users.noreply.github.com>
2018-12-28 22:49:35 +0300

    Add files via upload

---
+++ pwl.py
@@ -0,0 +1,326 @@
+print("\n\n\nHey! \nThis program can make you a password list.\nUp to 50 keys can be used.\nAll passwords will be saved in the documen passwords.txt in the same directory. \nVersion: 0.1\n\n28.12.2018/zahm\n\n\n")
+a=["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+i=0
+l = open('passwords.txt', 'w')
+while (i<50):
+    i=i+1
+    a[i]=input("Enter a keyword or leave the field blank: ")
+    pass
+i=0
+while (i<50):
+    i=i+1
+    l.write(a[1]+a[i]+"\n")
+    l.write(a[2]+a[i]+"\n")
+    l.write(a[3]+a[i]+"\n")
+    l.write(a[4]+a[i]+"\n")
+    l.write(a[5]+a[i]+"\n")
+    l.write(a[6]+a[i]+"\n")
+    l.write(a[7]+a[i]+"\n")
+    l.write(a[8]+a[i]+"\n")
+    l.write(a[9]+a[i]+"\n")
+    l.write(a[10]+a[i]+"\n")
+    l.write(a[11]+a[i]+"\n")
+    l.write(a[12]+a[i]+"\n")
+    l.write(a[13]+a[i]+"\n")
+    l.write(a[14]+a[i]+"\n")
+    l.write(a[15]+a[i]+"\n")
+    l.write(a[16]+a[i]+"\n")
+    l.write(a[17]+a[i]+"\n")
+    l.write(a[18]+a[i]+"\n")
+    l.write(a[19]+a[i]+"\n")
+    l.write(a[20]+a[i]+"\n")
+    l.write(a[21]+a[i]+"\n")
+    l.write(a[22]+a[i]+"\n")
+    l.write(a[23]+a[i]+"\n")
+    l.write(a[24]+a[i]+"\n")
+    l.write(a[25]+a[i]+"\n")
+    l.write(a[26]+a[i]+"\n")
+    l.write(a[27]+a[i]+"\n")
+    l.write(a[28]+a[i]+"\n")
+    l.write(a[29]+a[i]+"\n")
+    l.write(a[30]+a[i]+"\n")
+    l.write(a[31]+a[i]+"\n")
+    l.write(a[32]+a[i]+"\n")
+    l.write(a[33]+a[i]+"\n")
+    l.write(a[34]+a[i]+"\n")
+    l.write(a[35]+a[i]+"\n")
+    l.write(a[36]+a[i]+"\n")
+    l.write(a[37]+a[i]+"\n")
+    l.write(a[38]+a[i]+"\n")
+    l.write(a[39]+a[i]+"\n")
+    l.write(a[40]+a[i]+"\n")
+    l.write(a[41]+a[i]+"\n")
+    l.write(a[42]+a[i]+"\n")
+    l.write(a[43]+a[i]+"\n")
+    l.write(a[44]+a[i]+"\n")
+    l.write(a[45]+a[i]+"\n")
+    l.write(a[46]+a[i]+"\n")
+    l.write(a[47]+a[i]+"\n")
+    l.write(a[48]+a[i]+"\n")
+    l.write(a[49]+a[i]+"\n")
+    l.write(a[50]+a[i]+"\n")
+    pass
+f=0
+i=0
+while i<51:
+    i=i+1
+    while f<51:
+        f=f+1
+        l.write(a[1]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[2]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[3]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[4]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[5]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[6]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[7]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[8]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[9]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[10]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[11]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[12]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[13]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[14]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[15]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[16]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[17]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[18]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[19]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[20]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[21]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[22]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[23]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[14]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[24]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[25]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[26]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[27]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[28]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[29]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[30]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[31]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[32]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[33]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[34]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[35]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[36]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[37]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[38]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[39]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[40]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[41]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[42]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[43]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[44]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[45]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[46]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[47]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[48]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[49]+a[i]+a[f]+"\n")
+        pass
+    f=0
+    while f<51:
+        f=f+1
+        l.write(a[50]+a[i]+a[f]+"\n")
+        pass
+    f=0
+
+
+
+pass
+print("\nSuccess! Passwords have been saved to a file.\n")
